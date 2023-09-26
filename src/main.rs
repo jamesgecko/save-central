63529e59d906c6111a8001293212c8ecc582043d
James D <jamesgecko@users.noreply.github.com>
2023-09-25 21:38:22 -0400

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 // use std::fs;
+use csv::Error;
+use std::{env::var_os, path::PathBuf};
 
-fn main() {
-  if is_junction(r"C:\Users\james\AppData\Roaming\Baba_Is_You") {
-    println!("Junction");
-  } else {
-    println!("Not Junction");
-  }
+pub fn get_userprofile() -> String {
+    match var_os("USERPROFILE").map(PathBuf::from) {
+      Some(path) => {
+        return path.to_string_lossy().to_string();
+      }
+      None => {
+        panic!("Path does not exist");
+      }
+    }
 }
 
-fn is_junction(path: &str) -> bool {
+fn main() -> Result<(), Error> {
+  let mut reader = csv::Reader::from_path("smol_list.csv")?;
+  for record in reader.records() {
+    let record = record?;
+    let path = format!("{}\\{}", get_userprofile(), &record[0]);
+    println!("From {} to {}", path, &record[1]);
+
+    if is_junction(path) {
+      println!("Junction");
+    } else {
+      println!("Not Junction");
+      // move_and_junction
+      // hide_directory
+    }
+  }
+
+  Ok(())
+}
+
+fn is_junction(path: String) -> bool {
   let is_junction = junction::exists(path);
 
   match is_junction {
@@ -34,7 +58,7 @@ fn create_junction(old_path: &str, new_path: &str) {
 
 #[allow(dead_code)]
 fn move_save(old_path: &str, new_path: &str) -> std::io::Result<()> {
-  println!(format!("Moving... {} -> {}", old_path, new_path));
+  println!("{}", format!("Moving... {} -> {}", old_path, new_path));
   std::fs::rename(old_path, new_path)?;
   Ok(())
 }
